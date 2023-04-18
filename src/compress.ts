220e99704a840901003bc540f6dc3043d4f7cec4
changfeng <changfeng@gaoding.com>
2023-04-19 03:12:46 +0800

    refactor: simplify decompress code

--- src/compress.ts
+++ src/compress.ts
@@ -14,32 +14,30 @@ export async function compress(sourcePath: string, archivePath: string) {
         case 'zip':
         case 'vsix':
             if (isDirectory) {
-                compressing.zip.compressDir(sourcePath, archivePath, { ignoreBase: true });
+                await compressing.zip.compressDir(sourcePath, archivePath, { ignoreBase: true });
             } else {
-                compressing.zip.compressFile(sourcePath, archivePath, { ignoreBase: true });
+                await compressing.zip.compressFile(sourcePath, archivePath, { ignoreBase: true });
             }
             break;
         case 'gzip':
-            compressing.gzip.compressFile(sourcePath, archivePath, { ignoreBase: true });
+            await compressing.gzip.compressFile(sourcePath, archivePath, { ignoreBase: true });
             break;
         case 'tar':
             if (isDirectory) {
-                compressing.tar.compressDir(sourcePath, archivePath, { ignoreBase: true });
+                await compressing.tar.compressDir(sourcePath, archivePath, { ignoreBase: true });
             } else {
-                compressing.tar.compressFile(sourcePath, archivePath, { ignoreBase: true });
+                await compressing.tar.compressFile(sourcePath, archivePath, { ignoreBase: true });
             }
             break;
         case 'tgz':
             if (isDirectory) {
-                compressing.tgz.compressDir(sourcePath, archivePath, { ignoreBase: true });
+                await compressing.tgz.compressDir(sourcePath, archivePath, { ignoreBase: true });
             } else {
-                compressing.tgz.compressFile(sourcePath, archivePath, { ignoreBase: true });
+                await compressing.tgz.compressFile(sourcePath, archivePath, { ignoreBase: true });
             }
             break;
         case 'asar':
             await asar.createPackage(sourcePath, archivePath);
-            break;
-        default:
             break;
     }
 }
--- src/decompress.ts
+++ src/decompress.ts
@@ -1,57 +1,32 @@
-import fs from 'node:fs/promises';
 import path from 'node:path';
 
 import asar from 'asar';
 import compressing from 'compressing';
 
 import { decompressCrx } from './decompressCrx';
-import { pathExists } from './fsUtils';
 
 export async function decompress(archivePath: string, dest: string) {
     // .zip -> zip
     const archiveExt = path.extname(archivePath).toLowerCase().slice(1);
-    const destExt = path.extname(dest);
-    const tempFile = path.resolve(
-        path.dirname(dest),
-        `${path.basename(dest, destExt)}.decompressing${destExt}`,
-    );
-
-    async function clear() {
-        if (await pathExists(tempFile)) {
-            await fs.rm(tempFile, { recursive: true });
-        }
+    switch (archiveExt) {
+        case 'zip':
+        case 'vsix':
+            await compressing.zip.decompress(archivePath, dest);
+            break;
+        case 'tgz':
+            await compressing.tgz.decompress(archivePath, dest);
+            break;
+        case 'gzip':
+            await compressing.gzip.decompress(archivePath, dest);
+            break;
+        case 'tar':
+            await compressing.tar.decompress(archivePath, dest);
+            break;
+        case 'asar':
+            asar.extractAll(archivePath, dest);
+            break;
+        case 'crx':
+            await decompressCrx(archivePath, dest);
+            break;
     }
-
-    if (await pathExists(tempFile)) {
-        await fs.rm(tempFile, { recursive: true });
-    }
-
-    try {
-        switch (archiveExt) {
-            case 'zip':
-            case 'vsix':
-                await compressing.zip.decompress(archivePath, tempFile);
-                break;
-            case 'asar':
-                asar.extractAll(archivePath, tempFile);
-                break;
-            case 'tgz':
-                await compressing.tgz.decompress(archivePath, tempFile);
-                break;
-            case 'gzip':
-                await compressing.gzip.decompress(archivePath, tempFile);
-                break;
-            case 'tar':
-                await compressing.tar.decompress(archivePath, tempFile);
-                break;
-            // crx
-            default:
-                await decompressCrx(archivePath, tempFile);
-        }
-    } catch (error) {
-        await clear();
-        throw error;
-    }
-
-    await fs.rename(tempFile, dest);
 }
